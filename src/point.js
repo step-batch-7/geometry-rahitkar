5d3a4ea18bf7f95209bbcc9e3664635db156623f
Rahit <rahitkar@users.noreply.github.com>
2019-12-14 12:57:32 +0530

    added visit method with add

--- src/point.js
+++ src/point.js
@@ -1,11 +1,14 @@
-// p = new Point(2,3)// creates a point at 2,3
 class Point {
   constructor(x, y) {
     [this.x, this.y] = [x, y];
   }
 
   toString() {
-    return `point (${this.x}, ${this.y})`;
+    return `point @(${this.x}, ${this.y})`;
+  }
+
+  visit(func) {
+    return func(this.x, this.y);
   }
 }
 
--- test/pointTest.js
+++ test/pointTest.js
@@ -1,4 +1,3 @@
-// p = new Point(2,3)// creates a point at 2,3
 const assert = require("chai").assert;
 const Point = require("../src/point");
 
@@ -7,7 +6,17 @@ describe("point", () => {
     it("should give string representation of point", () => {
       const point = new Point(2, 4);
       const actual = point.toString();
-      const expected = `point (2, 4)`;
+      const expected = `point @(2, 4)`;
+      assert.strictEqual(actual, expected);
+    });
+  });
+
+  describe("visit", () => {
+    it("should give sum of two numbers when addition function is passed", () => {
+      const point = new Point(2, 4);
+      const add = (x, y) => x + y;
+      const actual = point.visit(add);
+      const expected = 6;
       assert.strictEqual(actual, expected);
     });
   });
